2f5e000e62bcec1daaec4082269f23d888e7b007
Alexander Kostenitskiy <kostenitskiy.alexander.work@gmail.com>
2022-11-02 17:02:58 +0200

    Правки

--- SelectSQLScript.sql
+++ SelectSQLScript.sql
@@ -1,5 +1,11 @@
 SELECT p.Name, c.Name FROM [MindBoxTest].[dbo].[ProductsCategories] AS pc
-FULL OUTER JOIN [MindBoxTest].[dbo].[Products] AS p
+LEFT JOIN [MindBoxTest].[dbo].[Products] AS p
 ON pc.ProductId = p.Id
 FULL OUTER JOIN [MindBoxTest].[dbo].[Categories] AS c
 ON pc.CategoryId = c.Id
+
+SELECT p.Name, c.Name FROM [MindBoxTest].[dbo].[Products] AS p
+LEFT JOIN [MindBoxTest].[dbo].[ProductsCategories] AS pc
+ON pc.ProductId = p.Id
+LEFT JOIN [MindBoxTest].[dbo].[Categories] AS c
+ON c.Id = pc.CategoryId
