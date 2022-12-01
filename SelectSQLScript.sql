b7e7073e59b27ab6db35b03dda91aa124c4b2dfd
Alexander Kostenitskiy <kostenitskiy.alexander.work@gmail.com>
2022-12-02 00:15:33 +0200

    Изменено название колонок у запроса

--- SelectSQLScript.sql
+++ SelectSQLScript.sql
@@ -1,4 +1,4 @@
-SELECT p.Name, c.Name FROM [MindBoxTest].[dbo].[Products] AS p
+SELECT p.Name as ProductName, c.Name as CategoryName FROM [MindBoxTest].[dbo].[Products] AS p
 LEFT JOIN [MindBoxTest].[dbo].[ProductsCategories] AS pc
 ON pc.ProductId = p.Id
 LEFT JOIN [MindBoxTest].[dbo].[Categories] AS c
